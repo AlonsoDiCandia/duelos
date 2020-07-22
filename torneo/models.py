18e374c9b2dd788b79d9c554ac1f3f7c31ee9ae2
Alonso Diaz Candia <hecdiaz@udec.cl>
2020-07-21 20:51:36 -0400

    Agregado Jugador

--- torneo/models.py
+++ torneo/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Jugador(models.Model):	
+	name = models.CharField(max_length=250)
+	duelos_jugados = models.IntegerField()
+	victorias = models.IntegerField()
+	derrotas = models.IntegerField()
+
+
+
+
+
